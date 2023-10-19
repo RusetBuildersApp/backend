b33831943e7e2e78aa0d5a5a7276f06de1ba3319
aLiyevTamerlan <tamerlan-aliyev-2021@mail.ru>
2023-10-19 14:45:30 +0400

    add random functionality

--- my_app/views/questions_views.py
+++ my_app/views/questions_views.py
@@ -1,3 +1,4 @@
+import random
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from my_app.pagination import QuestionPagination
@@ -7,7 +8,9 @@ from my_app.serializer.question_serializers import QuestionSerializer
 class GetQuestionAPIView(APIView, QuestionPagination):
     
     def get(self, request):
-        questions = Question.objects.all()
-        results = self.paginate_queryset(questions, request, view=self)
+        all_question_ids = Question.objects.values_list('id', flat=True)
+        random_question_ids = random.sample(list(all_question_ids), 5)
+        random_questions = Question.objects.filter(id__in=random_question_ids)
+        results = self.paginate_queryset(random_questions, request, view=self)
         serializer = QuestionSerializer(results, many=True)
         return self.get_paginated_response(serializer.data)
